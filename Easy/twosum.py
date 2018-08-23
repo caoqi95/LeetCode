d2737fa91dbfcbbf7120410852dca8a71c7ff21e
caoqiqi <caoqi95@gmail.com>
2018-08-23 13:02:15 +0800

    Update twosum.py

--- Easy/twosum.py
+++ Easy/twosum.py
@@ -1,3 +1,33 @@
+# solution 1
+# Time Limit Exceeded
+# return a list, [index1,index2]
+def twoSum(nums, target):
+    length = len(nums)
+    for i in range(length):
+        for j in range(i + 1, length):
+            if (nums[i] + nums[j] == target):
+                #print([i, j])
+                return [i, j]
+    return [0, 0]
+
+
+
+
+# solution 2
+# run time: 1127ms
+# return a list, [index1,index2]
+def twoSum(nums, target):
+    for i in range(len(nums)):
+        diff = target - nums[i]
+        if diff in nums and nums.index(diff) != i:
+            nums[i] = float('nan')
+            return [i, nums.index(diff)]
+
+
+
+
+# solusion 3, from other's solution
+# run time : 48ms
 # return a tuple, (index1, index2)
 def twoSum(nums, target):
     dict = {}
@@ -8,6 +38,3 @@ def twoSum(nums, target):
             return (dict[target-x], i)
         dict[x] = i
 
-
-if __name__ == '__main__':
-    twoSum([2,7,11,15], 9)
