b85274e931f47ab8874e85c702875aa0fbe81693
caoqiqi <caoqi95@gmail.com>
2018-08-27 21:21:45 +0800

    Update reverseInteger.py

--- Easy/reverseInteger.py
+++ Easy/reverseInteger.py
@@ -1,4 +1,4 @@
-# solution
+# solution 1, my own solution
 # run time: 116ms
 def reverse(x):
     """
@@ -11,19 +11,29 @@ def reverse(x):
         m = int(x_abs /(10**i))
         ones_place = m % 10
         a.append(ones_place)
-    #print(a)
+    # print(a)
     r = 0
     for i in range(0, len(a)):
         r += a[i]*10**(len(a)-(i+1))
     if x < 0 and -r >= -2**31:
-        #print(-r)
+        # print(-r)
         return -r
     elif x > 0 and r <= 2**31 -1 :
-        #print(r)
+        # print(r)
         return r
     else:
-        #print(0)
+        # print(0)
         return 0
 
-if __name__ == '__main__':
-    reverse(-32100)
+# solution 2, from other's solution，it's pythonic.
+# run time: 60ms
+    def reverse(x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        s = int(str(abs(x))[::-1])
+        # print(s)
+        if s > 2147483647 or s < -2147483648:
+            return 0
+        return s if x > 0 else -s
