7436fcf4dbe1fa566ffc404abeef9282930af65e
caoqiqi <caoqi95@gmail.com>
2018-11-04 23:43:18 +0900

    Update Search Insert Position.py

--- Easy/Search Insert Position.py
+++ Easy/Search Insert Position.py
@@ -8,20 +8,21 @@ class Solution:
         insert = 0
         if target in nums:
             insert = nums.index(target)
-            # print(insert)
+            print(insert)
+            return insert
+        elif nums[-1] < target:
+            insert = len(nums)
+            print(insert)
             return insert
         else:
             for i in range(len(nums)):
                 if nums[i]> target :
                     insert = i
-                    # print(insert)
+                    print(insert)
                     return insert
-        if nums[-1] < target:
-            insert = len(nums)
-        # print(insert)
-        return insert
+
 if __name__ == '__main__':
     nums = [1,3,5,6]
-    target = 7
+    target = 5
     solution = Solution()
     solution.searchInsert(nums, target)
