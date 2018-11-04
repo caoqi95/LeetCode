793498c7934da4bb3b5801eee04508576b5a863e
caoqi95 <caoqi95@gmail.com>
2018-11-04 23:38:29 +0900

    Update Easy/Search Insert Position.py

---
+++ Easy/Search Insert Position.py
@@ -0,0 +1,27 @@
+class Solution:
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        insert = 0
+        if target in nums:
+            insert = nums.index(target)
+            # print(insert)
+            return insert
+        else:
+            for i in range(len(nums)):
+                if nums[i]> target :
+                    insert = i
+                    # print(insert)
+                    return insert
+        if nums[-1] < target:
+            insert = len(nums)
+        # print(insert)
+        return insert
+if __name__ == '__main__':
+    nums = [1,3,5,6]
+    target = 7
+    solution = Solution()
+    solution.searchInsert(nums, target)
