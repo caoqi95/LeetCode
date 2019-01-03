00192f0abfb55839d5793834ae87c6d45fa2306a
caoqiqi <caoqi95@gmail.com>
2019-01-03 17:54:33 +0900

    Update #26-removeDuplicates.py

--- Easy/#26-removeDuplicates.py
+++ Easy/#26-removeDuplicates.py
@@ -1,15 +1,43 @@
-def removeDuplicates(nums):
-    """
-    :type nums: List[int]
-    :rtype: int
-    """
-    s = sorted(set(nums))
-    nums[:len(s)] = s
-    print(nums[:len(s)])
-    return len(s)
+"""
+Given a sorted array nums, remove the duplicates in-place such that each element appear only once and return the new length.
+
+Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+
+Example 1:
+
+Given nums = [1,1,2],
+
+Your function should return length = 2, with the first two elements of nums being 1 and 2 respectively.
+
+It doesn't matter what you leave beyond the returned length.
+Example 2:
+
+Given nums = [0,0,1,1,1,2,2,3,3,4],
+
+Your function should return length = 5, with the first five elements of nums being modified to 0, 1, 2, 3, and 4 respectively.
+
+It doesn't matter what values are set beyond the returned length.
+"""
+class Solution:
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if len(nums) == 0:
+            return 0
+
+        j = 1
+        for i in range(len(nums)):
+            if (i + 1 < len(nums)) and nums[i] < nums[i + 1]:
+                nums[j] = nums[i+1]
+                j += 1
+        print(nums)
+        print(j)
+        return j
 
 
-
-if __name__ == "__main__":
-    nums = [-1,0,0,0,0,3,3]
-    removeDuplicates(nums)
+if __name__ == '__main__':
+    nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4]
+    solution = Solution()
+    solution.removeDuplicates(nums)
