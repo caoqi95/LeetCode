8a0315947412717a191bc306104e37b6388fdc2c
caoqi95 <caoqi95@gmail.com>
2019-01-20 21:13:04 +0900

    Update #118

--- Easy/#118-Pascal's Triangle.py
+++ Easy/#118-Pascal's Triangle.py
@@ -1,12 +1,8 @@
 # coding=utf-8
 """
 Given a non-negative integer numRows, generate the first numRows of Pascal's triangle.
-
-
 In Pascal's triangle, each number is the sum of the two numbers directly above it.
-
 Example:
-
 Input: 5
 Output:
 [
