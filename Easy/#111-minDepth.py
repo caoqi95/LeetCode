bcc292b33c1e58ccb1deaa537ca969487252ce95
caoqi95 <caoqi95@gmail.com>
2019-01-20 19:45:33 +0900

    Add #111

---
+++ Easy/#111-minDepth.py
@@ -0,0 +1,49 @@
+"""
+Given a binary tree, find its minimum depth.
+
+The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+
+Note: A leaf is a node with no children.
+
+Example:
+
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+return its minimum depth = 2.
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def minDepth(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        # 采用迭代法
+        if not root:
+            return 0
+
+        children = [root.left, root.right]
+
+        # 如果根的下面没有任何子根
+        if not any(children):
+            return 1
+
+        # 'inf' 表示没有上限的初始化最大值，常用于找出最小值
+        min_depth = float('inf')
+        for c in children:
+            if c:
+                min_depth = min(self.minDepth(c), min_depth)
+        return min_depth + 1
