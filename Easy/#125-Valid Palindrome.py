29d7c566ea7a3579a61dd5acb68e794df64d74d2
caoqi95 <caoqi95@gmail.com>
2019-01-24 23:32:04 +0900

    Update #125

--- Easy/#125-Valid Palindrome.py
+++ Easy/#125-Valid Palindrome.py
@@ -16,12 +16,16 @@ Output: false
 import string
 # import operator # 用于判断两个字符串是否相等
 
+
 class Solution:
     def isPalindrome(self, s):
         """
         :type s: str
         :rtype: bool
         """
+
+        # 自己的解法，虽然长，但是好理解，耗时短
+
         if s is None:
             return True
 
@@ -48,11 +52,21 @@ class Solution:
         print(list_str == reserve_s)
         return list_str == reserve_s
 
+        """
+        # 别人的方法：短小精悍, 但是耗时长
+        
+        s = s.lower()
+        filtered = list(filter(lambda a: a.isalpha() or a.isdigit(), s))
+
+        print(filtered == filtered[::-1])
+        return filtered == filtered[::-1]
+        """
+
 
 if __name__ == '__main__':
 
-    # s = "A man, a plan, a canal: Panama"
-    s = "a car, a people"
+    s = "A man, a plan, a canal: Panama"
+    # s = "a car, a people"
     # s = " "
     solution = Solution()
     solution.isPalindrome(s)
