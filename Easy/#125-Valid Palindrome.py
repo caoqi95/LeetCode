58fb216df4f30dfd7cc3d99fb2db6b1d6138d5d9
caoqi95 <caoqi95@gmail.com>
2019-02-09 16:46:52 +0900

    Add #198

--- Easy/#125-Valid Palindrome.py
+++ Easy/#125-Valid Palindrome.py
@@ -1,6 +1,7 @@
 # coding=utf-8
 """
-Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and 
+ignoring cases.
 
 Note: For the purpose of this problem, we define empty string as valid palindrome.
 
