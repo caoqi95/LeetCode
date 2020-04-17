39a0ad72592f69935d6839269309fed0ffae056a
caoqi95 <caoqi95@gmail.com>
2020-04-17 21:17:44 +0900

    Add #985

---
+++ Easy/#985-Sum of Even Numbers After Queries.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""
+We have an array A of integers, and an array queries of queries.
+
+For the i-th query val = queries[i][0], index = queries[i][1], we add val 
+to A[index].  Then, the answer to the i-th query is the sum of the even values of A.
+
+(Here, the given index = queries[i][1] is a 0-based index, and each query permanently 
+modifies the array A.)
+
+Return the answer to all queries.  Your answer array should have answer[i] as 
+the answer to the i-th query.
+
+ 
+
+Example 1:
+
+Input: A = [1,2,3,4], queries = [[1,0],[-3,1],[-4,0],[2,3]]
+Output: [8,6,2,4]
+
+Explanation: 
+At the beginning, the array is [1,2,3,4].
+After adding 1 to A[0], the array is [2,2,3,4], and the sum of even values is 2 + 2 + 4 = 8.
+After adding -3 to A[1], the array is [2,-1,3,4], and the sum of even values is 2 + 4 = 6.
+After adding -4 to A[0], the array is [-2,-1,3,4], and the sum of even values is -2 + 4 = 2.
+After adding 2 to A[3], the array is [-2,-1,3,6], and the sum of even values is -2 + 6 = 4.
+"""
+
+class Solution(object):
+    def sumEvenAfterQueries(self, A, queries):
+        """
+        :type A: List[int]
+        :type queries: List[List[int]]
+        :rtype: List[int]
+        """
+        
+        """ Time Limit Exceeded
+        ans = []
+        ans_append = ans.append
+        for i in range(len(A)):
+            val = queries[i][0]
+            index  = queries[i][1]
+            A[index] += val
+            ans_append(sum(j for j in A if j % 2 == 0))
+            
+        return ans
+        """
+        
+        S = sum(x for x in A if x % 2 == 0)
+        ans = []
+        
+        for x, k in queries:
+            if A[k] % 2 == 0:
+                S -= A[k]
+            A[k] += x
+            if A[k] % 2 == 0:
+                S += A[k]
+            ans.append(S)
+        
+        return ans        
+        
+if __name__ == "__main__":
+    
+    A = [1, 2, 3, 4]
+    solution = Solution()
+    print(solution.sumEvenAfterQueries(A))
